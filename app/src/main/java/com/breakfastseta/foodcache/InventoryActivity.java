e64e29097b3f0c796aa40bc2b446798ad21523ac
Samuel Fang <samuelfangjw@gmail.com>
2020-06-02 00:46:27 +0800

    Added Sign in Feature

---
+++ app/src/main/java/com/breakfastseta/foodcache/InventoryActivity.java
@@ -0,0 +1,60 @@
+package com.breakfastseta.foodcache;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+
+        //Displaying user auth for debugging purposes
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String message = auth.toString();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+
+    public void delete(View view) {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+
+        //Displaying user auth for debugging purposes
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String message = auth.toString();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void signOut(View view) {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                public void onComplete(@NonNull Task<Void> task) {
+                    // ...
+                }
+            });
+
+        //Displaying user auth for debugging purposes
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String message = auth.toString();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/breakfastseta/foodcache/MainActivity.java
+++ app/src/main/java/com/breakfastseta/foodcache/MainActivity.java
@@ -1,91 +1,161 @@
 package com.breakfastseta.foodcache;
 
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private static final String KEY_INGREDIENT = "ingredient";
-    private static final String KEY_Expiry = "expiry_date";
-
-    private EditText editTextIngredient;
-    private EditText editTextExpiry;
-    private TextView textViewData;
-
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private DocumentReference itemRef = db.document("Items/firstitem");
+    private static final int RC_SIGN_IN = 100;
+    Button signInButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextIngredient =  findViewById(R.id.ingredient);
-        editTextExpiry =  findViewById(R.id.expiry_date);
-        textViewData = findViewById(R.id.result);
+
+        auth = FirebaseAuth.getInstance();
+        signInButton = findViewById(R.id.sign_in_button);
+
+        if (auth.getCurrentUser() != null) {
+            //Start signed in Activity
+            startMainActivity();
+        } else {
+            signInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    createSignInIntent();
+                }
+            });
+        }
     }
 
-    public void save(View view) {
-        String ingredient = editTextIngredient.getText().toString();
-        String expiry = editTextExpiry.getText().toString();
-
-        Map<String, Object> item = new HashMap<>();
-        item.put(KEY_INGREDIENT, ingredient);
-        item.put(KEY_Expiry, expiry);
-
-        itemRef.set(item)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Successfully Added Item To Database", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
-                    }
-                });
+    private void startMainActivity() {
+        Intent intent = new Intent(this, InventoryActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-    public void load(View view) {
-        itemRef.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            String Ingredient = documentSnapshot.getString(KEY_INGREDIENT);
-                            String Expiry = documentSnapshot.getString(KEY_Expiry);
+    public void createSignInIntent() {
+        // Firebase Authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
 
-                            textViewData.setText("Ingredient: " + Ingredient + "\n" + "Expiry Date: " + Expiry);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
-                    }
-                });
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+        }
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        Toast toast;
+        //Successfully signed in
+        if (resultCode == RESULT_OK) {
+            startMainActivity();
+        } else {
+            if (response == null) {
+                Toast.makeText(this, "Sign in Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Error signing in", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+//    public void signOut() {
+//        AuthUI.getInstance()
+//                .signOut(this)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
+//    }
+//
+//    public void delete() {
+//        AuthUI.getInstance()
+//                .delete(this)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
+//    }
+
+//    public void themeAndLogo() {
+//        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+//
+//        // [START auth_fui_theme_logo]
+//        startActivityForResult(
+//                AuthUI.getInstance()
+//                        .createSignInIntentBuilder()
+//                        .setAvailableProviders(providers)
+//                        .setLogo(R.drawable.my_great_logo)      // Set logo drawable
+//                        .setTheme(R.style.MySuperAppTheme)      // Set theme
+//                        .build(),
+//                RC_SIGN_IN);
+//        // [END auth_fui_theme_logo]
+//    }
+//
+//    public void privacyAndTerms() {
+//        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+//        // [START auth_fui_pp_tos]
+//        startActivityForResult(
+//                AuthUI.getInstance()
+//                        .createSignInIntentBuilder()
+//                        .setAvailableProviders(providers)
+//                        .setTosAndPrivacyPolicyUrls(
+//                                "https://example.com/terms.html",
+//                                "https://example.com/privacy.html")
+//                        .build(),
+//                RC_SIGN_IN);
+//        // [END auth_fui_pp_tos]
+//    }
+
 }
