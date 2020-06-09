e4b4f38209b4deb80d202fc9d579c657421c38db
Samuel Fang <samuelfangjw@gmail.com>
2020-06-09 19:46:38 +0800

    added barcode scanner (buggy)

--- app/src/main/java/com/breakfastseta/foodcache/ScanActivity.java
+++ app/src/main/java/com/breakfastseta/foodcache/ScanActivity.java
@@ -1,21 +1,49 @@
 package com.breakfastseta.foodcache;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.camerakit.CameraKitView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
 
 public class ScanActivity extends AppCompatActivity {
     private CameraKitView cameraKitView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         cameraKitView = findViewById(R.id.camera);
+        button = findViewById(R.id.button_detect);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
     }
 
     @Override
@@ -23,28 +51,94 @@ public class ScanActivity extends AppCompatActivity {
         super.onStart();
         cameraKitView.onStart();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         cameraKitView.onResume();
     }
+
     @Override
     protected void onPause() {
         cameraKitView.onPause();
         super.onPause();
     }
+
     @Override
     protected void onStop() {
         cameraKitView.onStop();
         super.onStop();
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    public void scan(View view) {
+//    private View.OnClickListener photoOnClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+//                @Override
+//                public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+//                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                    processImage(bitmap);
+//                }
+//            });
+//        }
+//    };
 
+    public void captureImage() {
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                processImage(bitmap);
+            }
+        });
+    }
+
+    private void processImage(Bitmap bitmap) {
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_UPC_A)
+                        .build();
+//        BarcodeScanner scanner = BarcodeScanning.getClient(options);
+        BarcodeScanner scanner = BarcodeScanning.getClient();
+        Task<List<Barcode>> result = scanner.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                        if (barcodes.isEmpty()) {
+                            Log.d("Scan", "No barcode Detected");
+                            captureImage();
+                        } else {
+                            Log.d("Scan", "Barcode Detected");
+                        }
+                        processBarcode(barcodes);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+    }
+
+    private void processBarcode(List<Barcode> barcodes) {
+        for (Barcode barcode : barcodes) {
+//            Rect bounds = barcode.getBoundingBox();
+//            Point[] corners = barcode.getCornerPoints();
+
+            String rawValue = barcode.getRawValue();
+
+            Log.d("Scan", "Barcode " +rawValue);
+            Toast.makeText(this, rawValue, Toast.LENGTH_SHORT).show();
+        }
     }
 }
